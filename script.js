f6cb92cef8164a013782ae3fa05d4fd510fecb46
aloosli <lizzwizz@tuta.io>
2023-01-07 14:55:13 +0000

    fixed generatePassword function to include all options choosen by user

--- script.js
+++ script.js
@@ -1,96 +1,98 @@
 // Array of special characters to be included in password
 const specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Array of numeric characters to be included in password
-const numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 const lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 // Array of uppercase characters to be included in password
 const upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
-
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  // Initialize an object to store the password options
+  let passwordOptions = {};
+  // Prompt the user for the password length
   let passwordLength;
   do {
     passwordLength = parseInt(
@@ -100,10 +102,29 @@ function getPasswordOptions() {
       alert("Please enter a valid number between 10 and 64.");
     }
   } while (isNaN(passwordLength) || passwordLength < 10 || passwordLength > 64);
-  return passwordLength;
+  passwordOptions.length = passwordLength;
+  // Prompt the user for the character types to include
+  let includeUppercase = confirm(
+    "Would you like to include uppercase letters in the password?"
+  );
+  passwordOptions.includeUppercase = includeUppercase;
+  let includeLowercase = confirm(
+    "Would you like to include lowercase letters in the password?"
+  );
+  passwordOptions.includeLowercase = includeLowercase;
+  let includeNumbers = confirm(
+    "Would you like to include numbers in the password?"
+  );
+  passwordOptions.includeNumbers = includeNumbers;
+
+  let includeSpecial = confirm(
+    "Would you like to include special characters in the password?"
+  );
+  passwordOptions.includeSpecial = includeSpecial;
+
+  // Return the password options object
+  return passwordOptions;  
 }
-
-
 
 // Function for getting a random element from an array
 function getRandom(arr) {
@@ -115,33 +136,52 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
-    // Calls the getPasswordOptions function
-    let passwordLength = getPasswordOptions();
-    // Initializes an empty array to store the password
-  let password = [];
-    // Generate a random password of the specified length
-    for (let i = 0; i < passwordLength; i++) {
-      // Select a random character from one of the arrays
-      let char = getRandom(upperCasedCharacters)
-        || getRandom(lowerCasedCharacters)
-        || getRandom(numericCharacters)
-        || getRandom(specialCharacters);
-      // Add the character to the password
-      password.push(char);
-    }
+  // Calls the getPasswordOptions function
+  let passwordOptions = getPasswordOptions();
 
+  // Initialize an array to store the character types to include in the password
+  let characterTypes = [];
+
+  // Push the appropriate character arrays to the characterTypes array based on the user's password options
+  if (passwordOptions.includeUppercase) {
+    characterTypes.push(upperCasedCharacters);
+  }
+  if (passwordOptions.includeLowercase) {
+    characterTypes.push(lowerCasedCharacters);
+  }
+  if (passwordOptions.includeNumbers) {
+    characterTypes.push(numericCharacters);
+  }
+  if (passwordOptions.includeSpecial) {
+    characterTypes.push(specialCharacters);
+  }
+
+  // Initializes an empty array to store the password
+  let password = [];
+  // Generate a random password of the specified length
+  for (let i = 0; i < passwordOptions.length; i++) {
+    // Select a random character type array
+    let characterType = getRandom(characterTypes);
+    // Select a random character from the selected character type array
+    let char = getRandom(characterType);
+    // Add the character to the password
+    password.push(char);
+  }
+  // Return the generated password
+  return password.join("");
 }
 
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  // Generate a new password
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
-
+  var passwordText = document.querySelector("#password");
+  // Write the generated password to the #password input
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener("click", writePassword);
